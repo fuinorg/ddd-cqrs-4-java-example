d26f1a7015198fba12894da910ae5c86c6f3a776
Michael Schnell <michael@fuin.org>
2019-12-25 19:19:02 +0100

    Removed native test for now

--- quarkus/query/src/test/java/org/fuin/cqrs4j/example/quarkus/query/NativePersonResourceIT.java
+++
@@ -1,9 +0,0 @@
-package org.fuin.cqrs4j.example.quarkus.query;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativePersonResourceIT extends PersonResourceTest {
-
-    // Execute the same tests but in native mode.
-}
