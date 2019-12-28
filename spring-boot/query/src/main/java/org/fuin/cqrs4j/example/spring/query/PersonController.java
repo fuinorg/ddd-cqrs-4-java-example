e44ea35ec14f74417817f1fcf0bde61d5fabb46b
Michael Schnell <michael@fuin.org>
2019-12-28 09:36:10 +0100

    Removed unused import

--- spring-boot/query/src/main/java/org/fuin/cqrs4j/example/spring/query/PersonController.java
+++ spring-boot/query/src/main/java/org/fuin/cqrs4j/example/spring/query/PersonController.java
@@ -19,7 +19,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
