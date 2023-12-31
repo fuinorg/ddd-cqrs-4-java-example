f96439740e57c0bb42b09b91e58cd2590f9e006c
Michael Schnell <michael@fuin.org>
2023-12-31 11:44:24 +0100

    Changed from HTTP to GRPC projection admin API

--- quarkus/command/src/main/java/org/fuin/cqrs4j/example/quarkus/command/api/ConstraintViolationExceptionMapper.java
+++ quarkus/command/src/main/java/org/fuin/cqrs4j/example/quarkus/command/api/ConstraintViolationExceptionMapper.java
@@ -1,22 +1,18 @@
 /**
  * Copyright (C) 2015 Michael Schnell. All rights reserved. http://www.fuin.org/
- *
+ * <p>
  * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
- *
+ * <p>
  * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU Lesser General Public License along with this library. If not, see
  * http://www.gnu.org/licenses/.
  */
 package org.fuin.cqrs4j.example.quarkus.command.api;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-
-import org.fuin.objects4j.common.Nullable;
+import jakarta.annotation.Nullable;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import jakarta.ws.rs.core.Context;
@@ -25,11 +21,14 @@ import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.Response.Status;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
-
 import org.fuin.cqrs4j.SimpleResult;
 import org.fuin.objects4j.common.Contract;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Maps the exceptions into a HTTP status.
--- quarkus/shared/src/main/java/org/fuin/cqrs4j/example/quarkus/shared/ProjectionAdminEventStoreFactory.java
+++ quarkus/shared/src/main/java/org/fuin/cqrs4j/example/quarkus/shared/ProjectionAdminEventStoreFactory.java
@@ -1,14 +1,12 @@
 package org.fuin.cqrs4j.example.quarkus.shared;
 
+import com.eventstore.dbclient.EventStoreDBClientSettings;
+import com.eventstore.dbclient.EventStoreDBProjectionManagementClient;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Disposes;
 import jakarta.enterprise.inject.Produces;
-import org.fuin.esc.admin.HttpProjectionAdminEventStore;
 import org.fuin.esc.api.ProjectionAdminEventStore;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.http.HttpClient;
+import org.fuin.esc.esgrpc.GrpcProjectionAdminEventStore;
 
 /**
  * CDI factory that creates a {@link ProjectionAdminEventStore} instance.
@@ -18,15 +16,16 @@ public class ProjectionAdminEventStoreFactory {
 
     @Produces
     @ApplicationScoped
-    public ProjectionAdminEventStore getProjectionAdminEventStore(final Config config, final HttpClient httpClient) {
-        final String url = config.getEventStoreProtocol() + "://" + config.getEventStoreHost() + ":" + config.getEventStoreHttpPort();
-        try {
-            final ProjectionAdminEventStore es = new HttpProjectionAdminEventStore(httpClient, new URL(url));
-            es.open();
-            return es;
-        } catch (final MalformedURLException ex) {
-            throw new RuntimeException("Failed to create URL: " + url, ex);
-        }
+    public ProjectionAdminEventStore getProjectionAdminEventStore(final Config config) {
+
+        final EventStoreDBClientSettings settings = EventStoreDBClientSettings.builder()
+                .addHost(config.getEventStoreHost(), config.getEventStoreHttpPort())
+                .defaultCredentials(config.getEventStoreUser(), config.getEventStorePassword())
+                .tls(false)
+                .buildConnectionSettings();
+        final EventStoreDBProjectionManagementClient client = EventStoreDBProjectionManagementClient.create(settings);
+        return new GrpcProjectionAdminEventStore(client).open();
+
     }
 
     /**
