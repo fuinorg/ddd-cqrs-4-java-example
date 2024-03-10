2454fff6cca76abf758f46584fc0ede7e1ccfdea
Michael Schnell <michael@fuin.org>
2024-03-10 16:53:56 +0100

    Develop (#16)

--- shared/src/main/java/org/fuin/cqrs4j/example/shared/PersonName.java
+++ shared/src/main/java/org/fuin/cqrs4j/example/shared/PersonName.java
@@ -19,6 +19,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import java.lang.annotation.*;
+
 /**
  * Name of a person.
  */
